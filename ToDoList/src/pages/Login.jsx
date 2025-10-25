884af622e8fb278d6d53eebd509df6d2d08700c3
anishbhanda <anishbca2020@gmail.com>
2025-10-25 17:17:08 +0530

    login submit form

--- ToDoList/src/pages/Login.jsx
+++ ToDoList/src/pages/Login.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+
+  const [form, setForm] = useState({
+    userName: "",
+    password: ""
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log("form submit");
+
+  }
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
       <div className="flex items-center justify-center">
-        <form className="bg-sky-500/30 p-8 rounded-lg text-white shadow-md w-60 sm:w-80 flex flex-col space-y-4">
+        <form className="bg-sky-500/30 p-8 rounded-lg text-white shadow-md w-60 sm:w-80 flex flex-col space-y-4" onSubmit={onSubmit}>
           <h2 className="text-2xl font-semibold text-center mb-2">Login</h2>
 
           <input
             type="text"
             placeholder="Username"
+            value={form.userName}
             className="p-2 rounded text-black focus:outline-none focus:ring-2 focus:ring-sky-300"
           />
 
